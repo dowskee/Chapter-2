77bb97a6ba063199a28ff7533b12c3cab12046dd
dowskee <31831031+dowskee@users.noreply.github.com>
2017-09-25 20:04:48 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
+using System.Reflection;
 
-namespace Ch02_Numbers
+namespace Ch02_Basics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:NO} to {int.MaxValue:NO}.");
-            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:NO} to {double.MaxValue:NO}.");
-            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:NO} to {decimal.MaxValue:NO}.");
+            System.Xml.XmlReader reader;
+            System.Xml.Linq.XElement element;
+            System.Net.Http.HttpClient client;
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies);
 
-            double a = 0.1M;
-            double b = 0.2M;
-            if (a + b == 0.3M) ;
             {
-                Console.WriteLine($"{a} + {b} equals 0.3");
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+                int methodCount = 0;
+                foreach (var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+                Console.WriteLine($"{a.DefinedTypes.Count():N0} types") + $"with {methodCount:N0} methods in {r.Name} assembly");
             }
-            else
-            {
-                Console.WriteLine($"{a} + {b} does NOT equal 0.3");
-            }
-            Console.ReadLine();
         }
     }
 }
